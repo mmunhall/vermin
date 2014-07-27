abf28909747f163f2e84f56c95736bb0ed36223a
Mike Munhall <munhall.mike@gmail.com>
2014-07-27 12:18:01 -0600

    Initial commit.

---
+++ vermin.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+var program = require('./node_modules/commander');
+// var gpio = require('./node_modules/pi-gpio');
+
+function sanitizeInterval(val) {
+    var interval = parseInt(val, 10);
+    if (isNaN(val)) {
+        console.log("***Invalid interval passed. Setting to the default.***");
+        return 5;
+    } else {
+        return interval;
+    }
+}
+
+function sanitizeEmail(val) {
+    var emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
+    if (!emailRegex.test(val)) {
+        console.log("***Invalid email passed. Setting to the default.***");
+        return "me@example.com";
+    } else {
+        return val;
+    }
+}
+
+program
+  .version('0.0.1')
+  .usage('test')
+  .option('-i, --interval [number]', 'Polling interval, in seconds', sanitizeInterval, 1)
+  .option('-e, --email [email]', 'Notification email address', sanitizeEmail, 'me@example.com')
+  .parse(process.argv);
+
+console.log('interval: %j', program.interval);
+console.log('email: %j', program.email);
+console.log("Running...");
