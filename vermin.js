27a97369dcc9dc2ea31d4caa993161023e562ef3
Mike Munhall <munhall.mike@gmail.com>
2014-08-03 16:52:30 -0600

    Added reset() method, doing some exploratory development.

--- vermin.js
+++ vermin.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 
 var program = require('./node_modules/commander');
-//var gpio = require('./node_modules/pi-gpio');
+var gpio = require('./node_modules/pi-gpio');
+
+var PIN_LED = 7;
 
 function sanitizeInterval(val) {
     var interval = parseInt(val, 10);
@@ -25,7 +27,7 @@ function sanitizeEmail(val) {
 
 program
   .version('0.0.1')
-  .usage('test')
+  .usage('./vermin.js')
   .option('-i, --interval [number]', 'Polling interval, in seconds', sanitizeInterval, 5)
   .option('-e, --email [email]', 'Notification email address', sanitizeEmail, 'me@example.com')
   .parse(process.argv);
@@ -34,16 +36,26 @@ console.log('interval: %j', program.interval);
 console.log('email: %j', program.email);
 console.log("Running...");
 
+function reset() {
+    gpio.open(PIN_LED, "output", function(err) {
+        gpio.write(PIN_LED, 0, function() {
+            gpio.close(PIN_LED);
+        });
+    });
+}
+
 function exec() {
 
-    gpio.open(7, "output", function(err) {
-        console.log(err);
-        gpio.write(7, 1, function() {
-            gpio.close(7);
+    var ledPinState = gpio.read(PIN_LED);
+
+    gpio.open(PIN_LED, "output", function(err) {
+        gpio.write(PIN_LED, 1, function() {
+            gpio.close(PIN_LED);
         });
     });
 
     setInterval(exec, program.interval * 1000);
 }
 
+// reset();
 exec();
