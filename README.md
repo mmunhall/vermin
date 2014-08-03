9a048ae8f156685637f9285847e976a447cc63f7
Mike Munhall <munhall.mike@gmail.com>
2014-08-02 21:36:01 -0600

    Updated README, Added test command.

--- README.md
+++ README.md
@@ -2,3 +2,22 @@ vermin
 ======
 
 A simple project for the Raspberry Pi to notify when a vermin has been captured by a mousetrap.
+
+
+Notes:
+install os (raspian)
+setup network config (etc/network/interfaces)
+    auto lo
+
+    iface lo inet loopback
+    iface eth0 inet dhcp
+
+    auto wlan0
+    allow-hotplug wlan0
+    iface wlan0 inet dhcp
+    	wpa-ssid "ssid"
+    	wpa-psk "password"
+
+install node
+install npm
+
--- vermin.js
+++ vermin.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 var program = require('./node_modules/commander');
-// var gpio = require('./node_modules/pi-gpio');
+//var gpio = require('./node_modules/pi-gpio');
 
 function sanitizeInterval(val) {
     var interval = parseInt(val, 10);
@@ -26,10 +26,23 @@ function sanitizeEmail(val) {
 program
   .version('0.0.1')
   .usage('test')
-  .option('-i, --interval [number]', 'Polling interval, in seconds', sanitizeInterval, 1)
+  .option('-i, --interval [number]', 'Polling interval, in seconds', sanitizeInterval, 5)
   .option('-e, --email [email]', 'Notification email address', sanitizeEmail, 'me@example.com')
   .parse(process.argv);
 
 console.log('interval: %j', program.interval);
 console.log('email: %j', program.email);
 console.log("Running...");
+
+function exec() {
+
+    gpio.open(7, "output", function(err) {
+        gpio.write(7, 1, function() {
+            gpio.close(7);
+        });
+    });
+
+    setInterval(exec, program.interval * 1000);
+}
+
+exec();
