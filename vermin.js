2e81b194d702d4e2f1b5fe3684f2b6908bf9f6b8
Mike Munhall <munhall.mike@gmail.com>
2014-08-05 20:17:08 -0600

    Changes input strategy.

--- vermin.js
+++ vermin.js
@@ -39,16 +39,16 @@ console.log("Running...");
 
 var monitor_state;
 
+gpio.open(PIN_MONITOR, "input");
+
 function exec() {
-    gpio.open(PIN_MONITOR, "input", function(err) {
-        gpio.read(PIN_MONITOR, function(err, value) {
-            if (err) {
-                console.log(err);
-                exit();
-            }
-            console.log(value);
-            monitor_state = value;
-        });
+    gpio.read(PIN_MONITOR, function(err, value) {
+        if (err) {
+            console.log(err);
+            exit();
+        }
+        console.log(value);
+        monitor_state = value;
     });
 
     gpio.open(PIN_LED, "output", function(err) {
