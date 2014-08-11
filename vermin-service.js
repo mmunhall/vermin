e7a4b4ff1901076447313c38b34d35a5785b49af
Mike Munhall <munhall.mike@gmail.com>
2014-08-11 17:45:20 -0600

    Implementation and cleanup of execute function, and a number of other cleanup items.

--- vermin-service.js
+++ vermin-service.js
@@ -1,19 +1,20 @@
 var gpio = require("./node_modules/pi-gpio");
 
-var LED_PIN          = 7,           // Pi pin number for LED circuit
-    MONITOR_PIN      = 11;          // Pi pin number for monitor circuit
-    MONITOR_INTERVAL = 2000;        // Number of milliseconds before LED blinks while monitoring trap
-    HIT_INTERVAL     = 250;         // Number of milliseconds before LED blinks once trap is triggered.
-    POLL_INTERVAL    = 1000;        // Number of milliseconds between polling the monitor circuit for its state
-    LOW		     = 0;           // LED LOW
-    HIGH	     = 1;	    // LED HIGH
-    currentState     = "monitoring" // One of "monitoring" or "hit". Initialized to "monitoring" at startup. Could mutate when polling.
+var LED_PIN                = 7,           // Pi pin number for LED circuit
+    MONITOR_PIN            = 11;          // Pi pin number for monitor circuit
+    MONITOR_LED_INTERVAL   = 2000;        // Number of milliseconds before LED blinks while monitoring trap
+    TRIGGERED_LED_INTERVAL = 250;         // Number of milliseconds before LED blinks once trap is triggered.
+    POLL_INTERVAL          = 1000;        // Number of milliseconds between polling the monitor circuit for its state
+    LOW		               = 0;           // LED LOW
+    HIGH	               = 1;           // LED HIGH
+    currentState           = "monitoring" // One of "monitoring" or "triggered". Initialized to "monitoring" at startup. Could mutate when polling.
+    executeIntervalId;                    // A handle on the setInterval() call.
 
 module.exports = {
     start: function () {
     	init();
     	cycleLedHigh();
-        setInterval(execute, POLL_INTERVAL);
+        pollIntervalId = setInterval(execute, POLL_INTERVAL);
     }
 }
 
@@ -22,24 +23,22 @@ function init () {
     gpio.open(LED_PIN, "output", function(err) { });
 }
 
-function execute () {    
+function execute () {
     gpio.read(MONITOR_PIN, function(err, value) {
-    	console.log(value);
-        //currentState = "hit";
+    	if (value === 0) {
+            currentState = "triggered";
+            notify();
+            clearInterval(executeIntervalId);
+        }
     });
-    //console.log(currentState);
 }
 
 function notify () {
-
-}
-
-function readTrap () {
-
+    console.log("notify!");
 }
 
 function cycleLedHigh () {
-    var interval = currentState === "monitoring" ? MONITOR_INTERVAL : HIT_INTERVAL;
+    var interval = currentState === "monitoring" ? MONITOR_LED_INTERVAL : TRIGGERED_LED_INTERVAL;
 
     setLed(HIGH, function () {
     	setTimeout(cycleLedLow, interval);
@@ -57,31 +56,3 @@ function setLed(value, callback) {
         if (callback) { callback(); }
     });
 }
-
-
-
-
-
-
-
-
-// function exec() {
-//     gpio.open(PIN_MONITOR, "input", function(err) {
-//         gpio.read(PIN_MONITOR, function(err, value) {
-//             console.log(value);
-//             monitor_state = value;
-//             gpio.close(PIN_MONITOR);
-//         });
-//     });
-//
-//     gpio.open(PIN_LED, "output", function(err) {
-//         gpio.write(PIN_LED, monitor_state, function() {
-//             console.log("monitor_state: " + monitor_state);
-//             gpio.close(PIN_LED);
-//         });
-//     });
-//
-//     setTimeout(exec, 2000);
-// }
-//
-// exec();
