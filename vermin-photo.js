2aef9c065885eaf1a37437510209b3f73b64d4f3
Mike Munhall <munhall.mike@gmail.com>
2014-08-28 23:25:15 +0000

    Deleted unused file.

--- vermin-photo.js
+++
@@ -1,9 +0,0 @@
-// var sys = require('sys');
-var exec = require('child_process').exec;
-
-module.exports = {
-    capture: function () {
-        var fileName = "img_" + new Date().valueOf() + ".jpg";
-        exec("raspistill -o imgCapture/" + fileName);
-    }
-}
